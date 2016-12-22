5441d1493fc0d608ebc0f22e3a94e86efd52ccf4
Milind Kadhi <milindkadhi@Milinds-MacBook-Pro.local>
2016-12-22 12:08:40 +0530

    First Commit

---
+++ Array.java
@@ -0,0 +1,18 @@
+class Array
+
+  {
+    public static void main(String ag[])
+	{
+          int n[]={5,6,2,1,4};
+	   int s=0;
+
+
+	   for(int p:n)
+	     {
+		s=s+p;
+		
+	      }
+		System.out.println("The Sum of Arrays is :- " +s);
+	}
+		   
+}
